387537bb659a52ff2f389d30b1c5e2644db2ee09
Zhenya Shepel <shepelzz@gmail.com>
2018-09-24 13:22:00 +0300

    lesson 2 hw

--- src/main/java/lesson2/homework2_2/Demo.java
+++ src/main/java/lesson2/homework2_2/Demo.java
@@ -1,7 +1,7 @@
 package lesson2.homework2_2;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Solution.increasePrice();
         //Solution.changeDescription();
     }
