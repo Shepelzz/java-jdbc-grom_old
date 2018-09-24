5950b1ad9aa726480d1329beb14760820b531988
Zhenya_home <shepelzz@gmail.com>
2018-09-24 21:43:38 +0300

    jdbs 4

--- src/main/java/lesson4/TransactionDemo.java
+++ src/main/java/lesson4/TransactionDemo.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionDemo {
@@ -14,9 +15,31 @@ public class TransactionDemo {
     private static final String PASS = "11111111";
     private static final String SQL_SAVE = "INSERT INTO PRODUCT VALUES(?, ?, ?, ?)";
 
-    public void save(List<Product> products) throws Exception{
-        try(Connection conn = getConnection(); PreparedStatement prpStmt = conn.prepareStatement(SQL_SAVE)){
-            conn.setAutoCommit(false);
+    public static void main(String[] args) throws Exception{
+        Product product1 = new Product(55, "name1", "test", 90);
+        Product product2 = new Product(66, "name2", "test1", 940);
+        Product product3 = new Product(66, "name3", "test232", 978);
+
+        List<Product> products = new ArrayList<>();
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+
+        save(products);
+    }
+
+    public static void save(List<Product> products) throws Exception{
+        try(Connection conn = getConnection()){
+            saveList(products, conn);
+        }catch (SQLException e){
+            System.err.println("Something went wrong");
+            throw e;
+        }
+    }
+
+    private static void saveList(List<Product> products, Connection connection) throws Exception{
+        try(PreparedStatement prpStmt = connection.prepareStatement(SQL_SAVE)){
+            connection.setAutoCommit(false);
 
             for(Product product : products) {
                 prpStmt.setLong(1, product.getId());
@@ -26,18 +49,17 @@ public class TransactionDemo {
 
                 if (prpStmt.executeUpdate() == 0)
                     throw new Exception("product with id " + product.getId() + " was not saved");
-
-
+                System.out.println("product "+product.toString()+" was saved");
             }
 
-            conn.commit();
+            connection.commit();
         }catch (SQLException e){
-            System.err.println("Something went wrong");
-            throw new SQLException(e);
+            connection.rollback();
+            throw e;
         }
     }
 
-    private Connection getConnection() throws SQLException{
+    private static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
