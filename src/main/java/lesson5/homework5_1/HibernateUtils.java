353a1614c545408821f28f385981253fa5c42a39
Zhenya Shepel <shepelzz@gmail.com>
2018-10-01 17:32:57 +0300

    lesson 5 hw

---
+++ src/main/java/lesson5/homework5_1/HibernateUtils.java
@@ -0,0 +1,12 @@
+package lesson5.homework5_1;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+    private SessionFactory sessionFactory;
+
+    public SessionFactory createSessionFactory(){
+        return new Configuration().configure().buildSessionFactory();
+    }
+}
